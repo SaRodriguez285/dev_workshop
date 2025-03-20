1f98859456827664130574a656576ff22bb2538f
SaRodriguez285 <santiago.pulidoa22@gmail.com>
2025-03-19 23:04:52 -0500

    Implementar Factorial

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -68,7 +68,13 @@ class Magic:
         Returns:
             int: El factorial de n
         """
-        pass
+        if n == 0 or n == 1:
+            return 1
+        resultado = 1
+        for i in range(2, n + 1):
+            resultado *= i
+        return resultado
+
     
     def mcd(self, a, b):
         """
